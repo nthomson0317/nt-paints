5906ae950b7c7d42bdcc65035671a49481ddc8e9
Nicholas Thomson <nicholasgraythomson@gmail.com>
2022-07-24 10:40:12 -0400

    react router dom v 6

--- src/App.js
+++ src/App.js
@@ -4,30 +4,39 @@ import About from './components/About'
 import Navbar from './components/Navbar'
 import Home from './components/Home'
 //REACT ROUTER
-import { Route, Switch, withRouter, Redirect} from 'react-router-dom'
+import { Route, Routes, withRouter, Redirect} from 'react-router-dom'
 
 //create basic templates for routes
 
-// const renderHome = (routerProps) => {
-//   return <Home />
-// }
+
 
 function App(props) {
+
+  let renderHome = (routerProps) => {
+    return 
+      <Home />
+      
+  }
+
+  let renderAbout = () => {
+    return
+    < About />
+  }
   console.log('app.js')
   return (
     <div>
         <Navbar
         history={props.history}/>
-        <Switch>
-        <Route exact path="/about" element={<About />} />
+        <Routes>
+        <Route path="/about" element={<About />} />
         <Route exact path="/" element={<Home />} />
-        </Switch>
+        </Routes>
 
       </div>
   );
 }
 // element={<About />}
-export default App;
+export default(App);
 
 
 
